21513e184b531072ba8a9ad46f2b9da45a1375fc
Bruno <bruno51_@hotmail.com>
2022-07-05 13:16:22 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include "cabecalho.html";?>
+
+  
+  <div class="container my-5">
+    <div class="bg-light p-5 rounded">
+      <div class="col-sm-8 py-5 mx-auto">
+        <h1 class="display-5 fw-normal">Texto</h1>
+        
+        <p>Texto</p>
+        <p>
+          
+        </p>
+    </div>
+    </div>
+  </div>
+</main>
+
+<?php include "rodape.html";?>
+
+
---
+++ view_Jogo.php
@@ -0,0 +1,18 @@
+<?php include "cabecalho.html";?>
+
+  
+  <div class="container my-5">
+    <div class="bg-light p-5 rounded">
+      <div class="col-sm-8 py-5 mx-auto">
+        <h1 class="display-5 fw-normal">Texto</h1>
+        
+        <p>Texto sobre o jogo</p>
+        <p>
+          jogo
+        </p>
+    </div>
+    </div>
+  </div>
+</main>
+
+<?php include "rodape.html";?>
